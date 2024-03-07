30d268665104ff42b603a46da0fc4187aa901e14
liudawei <liudawei@icsoc.net>
2024-03-07 15:21:00 +0800

    feat: create命令-第一步

--- bin/cli.js
+++ bin/cli.js
@@ -1,29 +1,24 @@
 #! /usr/bin/env node
 
-// #! 符号的名称叫 Shebang，用于指定脚本的解释程序
-// Node CLI 应用入口文件必须要有这样的文件头
-// 如果是Linux 或者 macOS 系统下还需要修改此文件的读写权限为 755
-// 具体就是通过 chmod 755 cli.js 实现修改
-console.log("creat-react-project working~");
-import spawn from "cross-spawn";
-import chalk from "chalk";
+import { program } from 'commander'
+import { createRequire } from "module";
+import createAction from '../lib/create.js'
+const require = createRequire(import.meta.url);
+const pkgJson = require('../package.json')
 
-// 定义需要安装的依赖
-const dependencies = ['react', 'react-dom', 'axios']
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exist')
+  .action((name, options)=>{
+    console.log('name:',name,' options:',options)
+    // 在create.js中执行穿甲任务
+    createAction(name,options)
+  })
 
-// 执行安装
-const child = spawn('npm',['install','-D'].concat(dependencies),{
-  stdio: 'inherit'
-})
+program
+  .version(`v${pkgJson.version}`)
+  .usage('<command> [option]')
 
-// 监听执行结果
-child.on('close',(code)=>{
-  if(code !== 0){
-    // 执行失败
-    console.log(chalk.red('Error occurred while installing dependencies'));
-    process.exit(1)
-  }else{
-    // 执行成功
-    console.log(chalk.cyan('Install finished'))
-  }
-})
+  // 解析用户执行命令时传入的参数
+program.parse(process.argv)
